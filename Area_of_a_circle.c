6cda73596bcff63a0b9681a661533f0aba6d5c65
Ganga-pavani <100119912+Ganga-pavani@users.noreply.github.com>
2022-04-22 22:48:42 +0530

    #include<stdio.h> int main() {     int r;     float area;     scanf("%d",&r);     area=3.14*r*r;     printf("%.2f",area);     return 0; }

--- Area_of_a_circle.c
+++ Area_of_a_circle.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
-#define PI 3.14
 int main()
 {
-    float r,x;
-  scanf("%f",&r);
-  x=3.14*r*r;
-  printf("%.2f",x);
-  return 0;
+    int r;
+    float area;
+    scanf("%d",&r);
+    area=3.14*r*r;
+    printf("%.2f",area);
+    return 0;
 }
