0e42f945205dbecb775762f1245753e416907732
Ganga-pavani <100119912+Ganga-pavani@users.noreply.github.com>
2022-04-25 17:14:33 +0530

    #include<stdio.h> int main() {     int n,sum=0,r;     scanf("%d",&n);    int temp=n;     while(temp>0)     {         r=temp%10;         sum=sum+r;         temp=temp/10;     }  if(n%sum==0)  printf ("True");  else  printf("False");  return 0; }

--- Harshed_number.c
+++ Harshed_number.c
@@ -1,22 +1,18 @@
 #include<stdio.h>
 int main()
 {
-    int n,r=0,sum=0,temp;
+    int n,sum=0,r;
     scanf("%d",&n);
-    temp=n;
+   int temp=n;
     while(temp>0)
     {
         r=temp%10;
         sum=sum+r;
         temp=temp/10;
     }
-    if(n%sum==0)
-   {
-       printf("True");
-   }
-    else
-    {
-      printf("False");
-    }
-    return 0;
+ if(n%sum==0)
+ printf ("True");
+ else
+ printf("False");
+ return 0;
 }
