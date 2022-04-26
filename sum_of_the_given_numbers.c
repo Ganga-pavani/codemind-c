4b074a5aa8c9ed4e04829f1de04171591ed0dc35
Ganga-pavani <100119912+Ganga-pavani@users.noreply.github.com>
2022-04-26 09:56:25 +0530

    #include<stdio.h> int main() { int t,c,a,b; scanf("%d",&t); while(t>0) {     scanf("%d %d",&a,&b);     c=a+b;     printf("%d",c);     printf(" ");     t--; } return 0; }

--- sum_of_the_given_numbers.c
+++ sum_of_the_given_numbers.c
@@ -1,23 +1,16 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,j,a[10][10],sum;
-    scanf("%d",&n);
-    for(i=0;i<n;i++)
-    {
-        for(j=0;j<2;j++)
-        {
-            scanf("%d",&a[i][j]);
-        }
-    }
-    for(i=0;i<n;i++)
-    {
-        sum=0;
-        for(j=0;j<2;j++)
-        {
-            sum=sum+a[i][j];
-        }
-        printf("%d
-",sum);
-    }
+int t,c,a,b;
+scanf("%d",&t);
+while(t>0)
+{
+    scanf("%d %d",&a,&b);
+    c=a+b;
+    printf("%d",c);
+    printf("
+");
+    t--;
+}
+return 0;
 }
