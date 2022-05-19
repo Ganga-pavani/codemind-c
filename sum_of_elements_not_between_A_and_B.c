36ec09e64dd9118b1070cbe24eb80e35276ac1eb
Ganga-pavani <100119912+Ganga-pavani@users.noreply.github.com>
2022-05-19 22:15:48 +0530

    #include<stdio.h> int main() {     int n,a[100],i,sum=0,x,y;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     scanf("%d%d",&x,&y);     for(i=0;i<n;i++)     {         if(a[i]>=x && a[i]<=y)         {             continue;         }         sum=sum+a[i];     }     printf("%d",sum);     return 0; }

---
+++ sum_of_elements_not_between_A_and_B.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int n,a[100],i,sum=0,x,y;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    scanf("%d%d",&x,&y);
+    for(i=0;i<n;i++)
+    {
+        if(a[i]>=x && a[i]<=y)
+        {
+            continue;
+        }
+        sum=sum+a[i];
+    }
+    printf("%d",sum);
+    return 0;
+}
